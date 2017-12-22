7b7d8cc02492e95a1e87b6cc359ae6483376f0d3
ManuNswamy <subramani.nswamy@gmail.com>
2017-12-22 14:00:49 +0530

    Leap User Andrioid/iOS

--- SharedCode/FirebaseHelper.cs
+++ SharedCode/FirebaseHelper.cs
@@ -3,17 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using System.Threading.Tasks;
 using Firebase.Xamarin.Database;
 using Firebase.Xamarin.Database.Query;
 
-namespace LeapUser
+namespace SharedCode
 {
     class FirebaseHelper
     {
